12450ba58412c674fb9156ccf0ea5006345e3b05
Samuel Martos (Casa) <samuelmv76@educastur.es>
2024-10-07 22:31:41 +0200

    a

---
+++ Practica1Unidad3/Ejercicio11.php
@@ -0,0 +1,8 @@
+<?php
+/*
+    Realizar un programa que muestre las películas que se han visto. Crear un array 
+    que  contenga  los  meses  de  enero,  febrero,  marzo  y  abril,  asignando  los  valores 
+    9,12,0  y  17  respectivamente.  Si  en  alguno  de  los  meses  no  se  ha  visto  alguna 
+    película no ha de mostrar la información de ese mes.
+ */
+?>
