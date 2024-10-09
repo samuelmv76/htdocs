f730177b45366e07cf83aeff3a07e50a6e51a119
Samuel Martos (clase) <samuelmv76@educastur.es>
2024-10-09 14:25:20 +0200

    Ejercicios Funciones Ejercicio2 sin terminar

---
+++ Practica3Unidad2/Ejercicio2.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Almacena la función anterior en el fichero matemáticas.php. Crea un fichero que 
+    la incluya y la utilice.
+*/
+
+
+
+?>
---
+++ Practica3Unidad2/matemáticas.php
@@ -0,0 +1,10 @@
+<?php
+include 'Ejercicio1.php';
+    
+for ($i=0; $i <1 ; $i++) { 
+    echo $i,"º resultado: ",ecuacion(1,2,-3)[$i];
+}
+
+
+    //echo "Primer resultado: ",ecuacion(1,2,-3)[0],"</br>","Segundo resultado: ", ecuacion(1,2,-3)[1];
+?>
