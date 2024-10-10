665c40b2e4a5c3bd27a85fed5cd7bdbccd71818a
Samuel Martos (clase) <samuelmv76@educastur.es>
2024-10-10 14:26:13 +0200

    Empezando Ejercicios

---
+++ Practica3Unidad2/Ejercicio4.php
@@ -0,0 +1,8 @@
+<?php
+/*
+    Escribe  una  función  que reciba  un  array  de números,  y  un número,  el  límite.  La 
+    función  tiene  que  devolver  un  nuevo  array  que  contenga  solo  los  elementos  del 
+    array original menores que el límite.
+*/
+$limite="";
+?>
