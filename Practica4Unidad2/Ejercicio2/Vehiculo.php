2f01e4c4cb91fbbbcc3ff74f83bf5c27fa87008e
Samuel Martos (clase) <samuelmv76@educastur.es>
2024-10-14 13:10:31 +0200

    Ejercicio 2 estructura

---
+++ Practica4Unidad2/Ejercicio2/Vehiculo.php
@@ -0,0 +1,12 @@
+<?php
+class Vehiculo {
+    public $color;
+    public $peso;
+    function circula() {
+        print "circula";
+    }
+    function añadir_persona($peso_persona) {
+        print "añadir persona";
+    }
+}
+?>
