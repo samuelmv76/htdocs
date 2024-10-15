89d248ddf1b7aad7791bb9a0ec6f807e3ae59ccf
Samuel Martos (clase) <samuelmv76@educastur.es>
2024-10-15 12:32:29 +0200

    Ejercicio24 terminado

--- Practica1Unidad3/Ejercicio23.php
+++ Practica1Unidad3/Ejercicio23.php
@@ -1,4 +1,12 @@
 <?php
+/*Crea  un  array  multidimensional  para  poder  guardar  los  componentes  de  dos 
+familias: “Los Simpson” y “Los Griffin” dentro de cada familia  ha  de  constar  el 
+padre,  la  madres  y  los  hijos,  donde  padre,  madre  e  hijos  serán  los  índices  y  los 
+índices y los nombres serán los valores. Esta estructura se ha de crear en un solo 
+array asociativo de tres dimensiones. 
+
+Muestra los valores de las dos familias en una lista no numerada.
+*/
 $familias = [
     "Los Simpson" => [
         "padre" => "Homer Simpson",
