99302b341a5d4988cac604731cd1e4ad88ac0631
Samuel Martos (Casa) <samuelmv76@educastur.es>
2024-10-15 16:57:11 +0200

    Ejercicio4 terminado

--- Practica3Unidad2/Ejercicio4.php
+++ Practica3Unidad2/Ejercicio4.php
@@ -4,5 +4,14 @@
     función  tiene  que  devolver  un  nuevo  array  que  contenga  solo  los  elementos  del 
     array original menores que el límite.
 */
-$limite="";
+$limite=7;
+$array=array(1,2,3,4,5,6,7,8,9,10);
+function arrayMenores($array, $limite){
+for ($i=0; $i < count($array); $i++) { 
+  if($array[$i]<$limite){
+    echo $array[$i], "<br>";
+  }
+}
+}
+arrayMenores($array, $limite);
 ?>
