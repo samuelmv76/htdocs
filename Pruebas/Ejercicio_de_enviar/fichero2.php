d705c805ce205ffe2c7ca50cfbc6d1ad81a73ddf
Samuel Martos (clase) <samuelmv76@educastur.es>
2024-10-17 14:22:47 +0200

    Pruebas clase

--- Pruebas/Ejercicio_de_enviar/fichero2.php
+++ Pruebas/Ejercicio_de_enviar/fichero2.php
@@ -1,38 +1,30 @@
 <?php
-if (isset($_POST['nombre']) && isset($_POST['apellidos'])){
-        echo $_POST['nombre'];
-        echo $_POST['apellidos'];
-    echo $name;
-    echo $ape;
-    /*
-    echo $_POST['nombre'];
-    echo "<br>";
-    echo $_POST['apellidos'];
-    */
+if (isset($_POST['nombre']) /*&& isset($_POST['apellidos'])*/){
+        echo "Nombre: ".$_POST['nombre']."</br>";
+        echo "Apellidos: ".$_POST['apellidos'];
     }
     else{
-        ?>
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <form action="fichero2.php" method="POST">
-        <label for="nombre">Nombre:</label><br>
-        <input type="text" id="nombre" name="nombre" ><br><br>
-        
-        <label for="apellidos">Apellidos:</label><br>
-        <input type="text" id="apellidos" name="apellidos" ><br><br>
-        
-        <input type="submit" value="Enviar">
-    
-    </form>
-</body>
-</html>
-<?php    
-}
+            ?>
+                <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Document</title>
+                    </head>
+                    <body>
+                        <form action="#" method="post">
+                            <label for="nombre">Nombre:</label><br>
+                            <input type="text" id="nombre" name="nombre" ><br><br>
+                            
+                            <label for="apellidos">Apellidos:</label><br>
+                            <input type="text" id="apellidos" name="apellidos" ><br><br>
+                            
+                                <!--<input type="submit" value="Enviar">-->
+                                <button type="submit">Enviar2</button>
+                        </form>
+                    </body>
+                </html>
+            <?php    
+        }
 ?>
---
+++ Pruebas/Ejercicio_de_enviar/test.php
@@ -0,0 +1,4 @@
+        <?php
+        echo $_POST['nombre'];
+        echo $_POST['apellidos'];
+        ?>
