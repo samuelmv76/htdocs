fb7c40c7516d6eaa3ed5597ea9e4aff6ff845ead
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-10-19 13:50:42 +0200

    Ejercicio 6 mostrar terminado

--- Practica4Unidad2/Ejercicio6/Coche.php
+++ Practica4Unidad2/Ejercicio6/Coche.php
@@ -6,7 +6,7 @@ include_once 'Cuatro_ruedas.php';
         public function añadir_persona($peso_persona) {
             parent::añadir_persona($peso_persona); // llama al método añadir_persona de la clase Cuatro_ruedas
             if ($this->peso >= 1500 && $this->numero_cadenas_nieve <= 2) {
-                echo "Atención, ponga 4 cadenas para la nieve.";
+                echo "Atención, ponga 4 cadenas para la nieve. </br>";
             }
         }
         /**
--- Practica4Unidad2/Ejercicio6/Mostrar.php
+++ Practica4Unidad2/Ejercicio6/Mostrar.php
@@ -4,5 +4,21 @@
     include_once 'Coche.php';
     include_once 'Dos_ruedas.php';
     include_once 'Cuatro_ruedas.php';
-    
+    /*
+    - Cree un coche verde de 2100 kg con 4 puertas en la página mostrar.php. 
+    - Añada 2 cadenas para la nieve y una persona de 80 kg. 
+    - Cambie el color del coche a azul. 
+    - Quite 4 cadenas para la nieve. 
+    - Vuelva a pintar el coche en color negro. 
+    -Muestre todos los atributos del coche y el número de veces que se cambia 
+    el color con el método ver_atributo($objeto). 
+    - El nuevo modelo es (los accesos no se representan):
+    */
+    $coche = new Coche("verde",2100,4);
+    $coche->añadir_cadenas_nieve(2);
+    $coche->añadir_persona(80);
+    $coche->repintar("azul");
+    $coche->quitar_cadenas_nieve(4);
+    $coche->repintar("negro");
+    $coche->ver_atributo($coche);
 ?>
