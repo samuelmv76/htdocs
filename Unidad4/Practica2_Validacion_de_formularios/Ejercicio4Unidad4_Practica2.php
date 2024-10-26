c18a6d3e78341d7f4855a1efadbe9da3344e85f2
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-10-27 00:04:24 +0200

    Ejercicio4Unidad4_Practica2

---
+++ Unidad4/Practica2_Validacion_de_formularios/Ejercicio4Unidad4_Practica2.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    ¿Qué acción realizará la siguiente línea de código HTML? 
+    Sexo: <input type="radio" name="sexo"
+      <?php if (isset($sexo) && $sexo=="mujer") echo "checked";?>
+        value="mujer"> Mujer <input type="radio" name="sexo"  
+        <?php if (isset($sexo) && $sexo=="hombre") echo "checked";?>
+         value="hombre"> Hombre <span class="error">*
+          <?php echo $sexoErr;?></span><br><br> 
+
+          Este código permite que el usuario seleccione su género utilizando botones de radio,
+          y muestra un mensaje de error en caso de que la selección no sea válida.
+*/
+
+?>
