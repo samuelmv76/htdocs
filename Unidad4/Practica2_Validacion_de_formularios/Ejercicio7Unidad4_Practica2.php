97528395324cc75cb2ae7cd7811e6cd4f9e22bf3
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-11-02 21:10:03 +0100

    Ejercicio 7 terminado

---
+++ Unidad4/Practica2_Validacion_de_formularios/Ejercicio7Unidad4_Practica2.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+    Las expresiones regulares en PHP, qué son y para qué sirven?:
+
+    Una expresión regular es básicamente una secuencia de 
+    caracteres que define un patrón de búsqueda, y en PHP se utilizan principalmente 
+    para buscar y reemplazar patrones en strings.
+*/
+?>
