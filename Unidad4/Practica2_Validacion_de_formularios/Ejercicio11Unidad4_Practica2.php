e3b7c1c2fee1d258e7547bc44238d7f427e5d9ff
Samuel Martos (Clase) <samuelmv04@gmail.com>
2024-11-04 12:52:38 +0100

    EJercicio11Unidad4_Practica2

---
+++ Unidad4/Practica2_Validacion_de_formularios/Ejercicio11Unidad4_Practica2.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Crear el siguiente formulario: a. Los campos nombre, e-mail, y sexo se han de introducir de forma 
+    obligatoria. b. Se ha de indicar con el mensaje en rojo “*campos requeridos” así como un 
+    * al lado de nombre, e-mail y sexo. c. El nombre únicamente ha de admitir letras y espacios en blanco d. Email debe de tener un valor correcto. e. El sitio web debe de tener un valor correcto El resultado (valores de entrada en el formulario) debe de mostrar los datos introducidos 
+    sean correctos o no.
+*/
+
+?>
