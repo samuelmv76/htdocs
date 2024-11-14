82209a9ca732dcd24470795e5e132801df179ec8
Samuel Clase(puestoDario) <samuelmv04@gmail.com>
2024-11-14 13:41:02 +0100

    ejercicio 1 le falta la validacion

---
+++ parcial_Samuel_Martos/ejercicio1/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <?php
+        session_start();
+        $cont=0;
+        if(isset($_POST['calcular'])){
+            //validar
+            for ($i=0;$i<$cont;$i++){
+                
+            }
+            if($_POST["valor$i"]>100 || $_POST["valor$i"]<1){
+                echo'El valor tiene que estar entre 1 y 100';
+            }else{
+                //resultado
+                for ($i=1; $i < 7; $i++) { 
+                    $variable[$i]=$_POST["valor$i"];
+                    echo $variable[$i]." = ".decbin($variable[$i])."<br>";
+                }   
+            }
+        }else{
+            for ($i=0; $i < 3; $i++) { echo '<br>';
+                for ($j=0; $j <2 ; $j++) { 
+                    $cont++;
+                        echo '<label for="E.'.$i,$j.'">E.'.$i.",".$j.':</label>';
+                        echo '<input type="text" id="numero" name="valor'.$cont.'">';
+                    }
+            }
+            
+            echo '<br>';
+            echo'<button class="button" name="calcular" type="submit">Calcular</button>';
+        }
+        ?>
+    </form>
+</body>
+</html>
