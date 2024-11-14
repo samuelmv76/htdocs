0f63c8f5628494d9f5e7e584696db084e4ea643e
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-11-14 21:34:41 +0100

    Ejercicio 3 terminado

--- parcial_Samuel_Martos/ejercicio3/index.php
+++ parcial_Samuel_Martos/ejercicio3/index.php
@@ -9,35 +9,46 @@
     <form action="#" method="post">
         <?php
         session_start();
-        $cont=0;
         
-        if(isset($_POST['submit'])){
-                //resultado
-                $numero_aleatorio=$_SESSION['numero'];
-                $tu_num=$_POST['valor'];
-                echo '<p>Tu número es: '.$tu_num.'</p>';
-                
-                if($numero_aleatorio==$tu_num){
-                    echo'ENHORABUENA, HAS ACERTADO';
-                    echo'Has necesitado '.$cont.' intentos';
-                }else{
-                    if($numero_aleatorio>$tu_num){
-                        echo'<p>Mi número es MAYOR</p>';
-                    }else{
-                        echo'<p>Mi número es MENOR</p>';
-                    }
-                    echo '<a href="index.php">Sigue jugando...</a>';
-                    $cont++;
-                }
-
-        }else{
+        if(!isset($_SESSION['numero'])) {
             $numero=rand(1,100);
+            $_SESSION['numero']=$numero;
+            $_SESSION['cont']=0;
+        }
+        
+        $numero_aleatorio=$_SESSION['numero'];
+        $cont=$_SESSION['cont'];
+        
+        if(isset($_POST['submit'])) {
+            //resultado
+            $tu_num=$_POST['valor'];
+            echo '<p>Tu número es: '.$tu_num.'</p>';
+            
+            if($numero_aleatorio==$tu_num) {
+                echo'ENHORABUENA, HAS ACERTADO <br>';
+                echo'Has necesitado '.$cont.' intentos';
+                // Reiniciar el contador de intentos
+                $_SESSION['cont']=0;
+            } else {
+                if($numero_aleatorio>$tu_num) {
+                    echo'<p>Mi número es MAYOR</p>';
+                } else {
+                    echo'<p>Mi número es MENOR</p>';
+                }
+                echo '<a href="index.php">Sigue jugando...</a>';
+                // Incrementar el contador de intentos
+                $_SESSION['cont']++;
+                $cont=$_SESSION['cont'];
+            }
+        } else {
             echo '<label for="">Adivina mi número:</label>';
             echo '<input type="text" id="numero" name="valor">';
-            $_SESSION['numero']=$numero;
             echo '<br>';
             echo'<button class="button" name="submit" type="submit">Enviar</button>';
         }
+        
+        // Mostrar el número de intentos
+        echo '<p>Intentos: '.$cont.'</p>';
         ?>
     </form>
 </body>
