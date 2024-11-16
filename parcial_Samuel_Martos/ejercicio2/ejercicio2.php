c0c266845a807617cb9e16c494eda5af09f28e99
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-11-16 20:33:20 +0100

    Cartas funcionando

--- parcial_Samuel_Martos/ejercicio2/ejercicio2.php
+++ parcial_Samuel_Martos/ejercicio2/ejercicio2.php
@@ -7,6 +7,53 @@
 </head>
 <body>
     <h1>Adivina el número decimal</h1>
-    <h2>EL numero en binario</h2>
+    <h2>El numero en binario es: <?php 
+    session_start();
+    for ($i=0; $i <4 ; $i++) { 
+        $num[$i]=rand(0,1);
+    }
+    //imprimir los numeros del array
+    foreach ($num as $key => $value) {
+        echo $value;
+    }
+  
+    ?></h2>
+    <?php
+        //Calcular, utilizando un segundo vector 
+        //que inicializamos con potencias de dos, el número asociado en decimal.
+        for ($i=4; $i >= 0; $i--) { 
+            $potencias[$i]=2**$i;
+        }
+        //Representación gráfica de las cartas. (1 punto)
+        for ($i=0; $i < 4; $i++) { 
+            if($num[$i]==1){
+                //pinta la carta
+                //echo '<img src="Uno.jpg">';
+                //saber que carta pintar
+                    //mirar en que potencia esta
+                    switch ($potencias[$i]) {
+                        case 1:
+                            echo '<img src="Uno.jpg">';
+                            break;
+                            case 2:
+                                echo '<img src="dos.jpg">';
+                                break;
+                                case 4:
+                                    echo '<img src="cuatro.jpg">';
+                                    break;
+                                    case 8:
+                                        echo '<img src="ocho.jpg">';
+                                        break;
+                    }
+            }else{
+                echo '<img src="blanco.jpg">';
+            }
+        }
+?>     
+    <form action="ejercicio21.php" method="post">
+        <label for="nombre">Numero decimal: </label>
+        <input type="text" id="numdecimal" name="numdecimal" required><br><br>
+        <input type="submit" name="submit" value="submit">
+    </form>
 </body>
 </html>
