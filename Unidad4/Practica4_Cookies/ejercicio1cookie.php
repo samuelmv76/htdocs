b72f4c6de5f121eb69f745136ad785a49b45c12b
Samuel Martos(clase) <samuelmv04@gmail.com>
2024-11-18 14:24:40 +0100

    ejercicio1 empezado

--- Unidad4/Practica4_Cookies/ejercicio1cookie.php
+++ Unidad4/Practica4_Cookies/ejercicio1cookie.php
@@ -8,10 +8,13 @@
 <body>
    <p>Seleccione de que color desea que sea la web de ahora en adelante:</p>
         <form action="ejercicio1cookie.php" method="post">
-            <input type="button" id="rojo" name="rojo">
-            <input type="button" id="verde" name="verde">
-            <input type="button" id="azul" name="azul">
-
+            <input type="radio" id="rojo" name="rojo">
+            <label>rojo</label>
+            <input type="radio" id="verde" name="verde">
+            <label>verde</label>
+            <input type="radio" id="azul" name="azul">
+            <label>azul</label>
+            <br>
             <input type="submit" name="submit" value="Crear cookie">
             <?php 
                 session_start();        
