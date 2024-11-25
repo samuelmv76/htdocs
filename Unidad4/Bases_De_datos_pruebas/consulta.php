482ece250849f84d17eb265a680df32e60b0bcdc
Samuel Martos(clase) <samuelmv04@gmail.com>
2024-11-25 14:27:13 +0100

    bbdd

---
+++ Unidad4/Bases_De_datos_pruebas/consulta.php
@@ -0,0 +1,22 @@
+<?php // query-mysqli.php
+    require_once 'login.php';
+    $connection = new mysqli($hn, $un, $pw, $db);
+    if ($connection->connect_error) die("Fatal Error");
+
+    $query = "SELECT usu, contra FROM usuarios"; 
+    $result = $connection->query($query);
+    if (!$result) die("Fatal Error");
+
+    $rows = $result->num_rows;
+
+    for ($j = 0 ; $j < $rows ; ++$j) {
+        $row = $result->fetch_assoc();
+        echo 'ID: ' . htmlspecialchars($row['id']) . '<br>';
+        echo 'Username: ' . htmlspecialchars($row['usu']) . '<br>';
+        echo 'Contra: ' . htmlspecialchars($row['contra']) . '<br>';
+        echo 'Rol: ' . htmlspecialchars($row['rol']) . '<br><br>';
+    }
+
+    $result->close();
+    $connection->close();
+?> 
---
+++ Unidad4/Bases_De_datos_pruebas/login.php
@@ -0,0 +1,6 @@
+<?php // login.php
+ $hn = 'localhost:3307';
+ $db = 'bdsimon';
+ $un = 'jugador';
+ $pw = 'jugador';
+?>
