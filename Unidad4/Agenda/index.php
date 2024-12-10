ff75ad25359770b8f7d0735320d2d8471254320b
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-12-10 18:04:54 +0100

    agenda

---
+++ Unidad4/Agenda/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>AGENDA DE CONTACTOS</h1>
+        <form action ="inicio.php" method ="post">
+            <label>Usuario:</label>
+            <input type="text" name="usu" required>
+            <br>
+            <label>Contraseña:</label>
+            <input type="password" name="contra" required>
+            <br>
+            <button type="submit" name='submit'>Entrar</button>
+        </form>
+</body>
+</html>
---
+++ Unidad4/Agenda/login.php
@@ -0,0 +1,6 @@
+<?php // login.php
+ $hn = 'localhost:3307';/*:3307 o vacio*/
+ $un = 'root';
+ $pw = '';
+ $db = 'agenda';
+?>
