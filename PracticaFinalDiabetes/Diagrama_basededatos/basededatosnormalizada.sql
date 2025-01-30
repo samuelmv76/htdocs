a34aa486438f3e7288bfb3dd209114775aaa29aa
Samuel Clase puesto dani <samuelmv04@gmail.com>
2025-01-30 14:05:44 +0100

    base de datos

--- PracticaFinalDiabetes/Diagrama_basededatos/basededatosnormalizada.sql
+++ PracticaFinalDiabetes/Diagrama_basededatos/basededatosnormalizada.sql
@@ -7,49 +7,38 @@ CREATE TABLE Usuarios (
     apellidos VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     contrasena VARCHAR(255) NOT NULL,
-    fechaNacimiento DATE NOT NULL
+    fecha_nacimiento DATE NOT NULL
 );
 
 CREATE TABLE Control_Glucosa (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
-    fecha DATE NOT NULL UNIQUE,
-    actividad TINYINT NOT NULL, -- del 1 al 5
+    fecha DATE NOT NULL,    
+    actividad TINYINT NOT NULL CHECK (actividad BETWEEN 1 AND 5),
     insulina_lenta INT NOT NULL,
+    PRIMARY KEY (usuario_id, fecha),
     FOREIGN KEY (usuario_id) REFERENCES Usuarios(id)
 );
 
 CREATE TABLE Comida (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    control_glucosa_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    fecha DATE NOT NULL,
     tipo_comida ENUM('DESAYUNO', 'COMIDA', 'CENA') NOT NULL,
     raciones DECIMAL(5,2) NOT NULL,
     glucosa_1h INT NOT NULL,
     insulina INT NOT NULL,
     glucosa_2h INT NOT NULL,
-    FOREIGN KEY (control_glucosa_id) REFERENCES Control_Glucosa(id)
+    PRIMARY KEY (usuario_id, fecha, tipo_comida),
+    FOREIGN KEY (usuario_id, fecha) REFERENCES Control_Glucosa(usuario_id, fecha)
 );
 
-CREATE TABLE Hipo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Episodios_Glucosa (
     usuario_id INT NOT NULL,
     fecha DATE NOT NULL,
     tipo_comida ENUM('DESAYUNO', 'COMIDA', 'CENA') NOT NULL,
     hora TIME NOT NULL,
     glucosa INT NOT NULL,
     correccion INT NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES Usuarios(id),
-    FOREIGN KEY (fecha) REFERENCES Control_Glucosa(fecha)
-);
-
-CREATE TABLE Hiper (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT NOT NULL,
-    fecha DATE NOT NULL,
-    tipo_comida ENUM('DESAYUNO', 'COMIDA', 'CENA') NOT NULL,
-    hora TIME NOT NULL,
-    glucosa INT NOT NULL,
-    correccion INT NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES Usuarios(id),
-    FOREIGN KEY (fecha) REFERENCES Control_Glucosa(fecha)
+    tipo ENUM('HIPO', 'HIPER') NOT NULL,
+    PRIMARY KEY (usuario_id, fecha, tipo_comida, hora, tipo),
+    FOREIGN KEY (usuario_id, fecha) REFERENCES Control_Glucosa(usuario_id, fecha)
 );
