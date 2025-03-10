4a0f48cad84651dab9a725dbdd877ddeff0203a6
Samuel Clase puesto dani <samuelmv04@gmail.com>
2025-03-10 14:20:40 +0100

    Show y edit con plantilla

--- laravel/Videoclub_final/resources/views/catalog/edit.blade.php
+++ laravel/Videoclub_final/resources/views/catalog/edit.blade.php
@@ -1,34 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>VideoClub</title>
-</head>
-<body>
-<!-- resources/views/catalog/edit.blade.php -->
+@extends('layouts.master')
 
-<h1>Editar Película</h1>
+@section('content')
+    <h1>Editar Película</h1>
 
-@if ($pelicula)
-    <p><strong>Título:</strong> {{ $pelicula->title }}</p>
-    <p><strong>Año:</strong> {{ $pelicula->year }}</p>
-    
-    <!-- Puedes agregar más campos aquí si la película tiene más información -->
-    
-    <form action="" method="POST">
-        @csrf
-        @method('PUT')
+    @if ($pelicula)
+        <p><strong>Título:</strong> {{ $pelicula->title }}</p>
+        <p><strong>Año:</strong> {{ $pelicula->year }}</p>
         
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" value="{{ $pelicula->title }}" required>
-        
-        <label for="anio">Año:</label>
-        <input type="number" name="anio" value="{{ $pelicula->year }}" required>
-        
-        <button type="submit">Actualizar</button>
-    </form>
-@else
-    <p>La película no se encuentra.</p>
-@endif
-</body>
-</html>
+        <form action="" method="POST">
+            @csrf
+            @method('PUT')
+            
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" value="{{ $pelicula->title }}" required>
+            
+            <label for="anio">Año:</label>
+            <input type="number" name="anio" value="{{ $pelicula->year }}" required>
+            
+            <button type="submit">Actualizar</button>
+        </form>
+    @else
+        <p>La película no se encuentra.</p>
+    @endif
+@stop
--- laravel/Videoclub_final/resources/views/catalog/show.blade.php
+++ laravel/Videoclub_final/resources/views/catalog/show.blade.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>VideoClub</title>
-</head>
-<body>
-<!-- resources/views/catalog/show.blade.php -->
+@extends('layouts.master')
 
-<h1>Detalles de la Película</h1>
+@section('content')
+    <h1>Detalles de la Película</h1>
 
-
-
-<h2>Lista de Películas</h2>
-<ul>
-    @foreach ($arrayPeliculas as $pelicula)
-        <li>{{ $pelicula->title }} - {{ $pelicula->year }}</li>  <!-- Cambia 'nombre' y 'anio' por los campos reales de la tabla -->
-    @endforeach
-</ul>
-
-</body>
-</html>
+    <h2>Lista de Películas</h2>
+    <ul>
+        @foreach ($arrayPeliculas as $pelicula)
+            <li>{{ $pelicula->title }} - {{ $pelicula->year }}</li>
+        @endforeach
+    </ul>
+@stop
